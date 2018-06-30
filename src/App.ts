37e3d06eee146d69edc03615a75d375f0fd5c8c0
Milan Bharanya <milan.bharanya@tundra.com>
2018-06-30 13:53:15 +0200

    Try stuff before REST rewrite

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
 import * as WebRequest from 'web-request'
+import * as jQuery from 'jquery'
 import * as cheerio from 'cheerio'
 export default class App {
     private static readonly QUERY_REPLACE_STRING = "$searchQuery"
@@ -7,38 +8,61 @@ export default class App {
         + App.QUERY_REPLACE_STRING + "&lang=de&treffertyp=rezepte&start="
         + App.START_INDEX_REPLACE_STRING + "&num=1"
 
-    async display(): Promise<any> {
-        const keyword = "vegan"
+    async getRandomRecipeCard(): Promise<any> {
+        const keyword = "vegetarisch"
 
-        const searchQuery = App.API_URL.replace(App.QUERY_REPLACE_STRING, keyword).replace(App.START_INDEX_REPLACE_STRING, "0");
+        const defaultStartIndex = "0";
+        const searchQuery = App.API_URL.replace(App.QUERY_REPLACE_STRING, keyword).replace(App.START_INDEX_REPLACE_STRING, defaultStartIndex);
         const response = await WebRequest.get(searchQuery)
         const foobyResults = <FoobySearchResponse>JSON.parse(response.content)
         const randomIndex = Math.floor(Math.random() * foobyResults.resultcounts.all)
         console.debug(randomIndex)
 
-        const randomResponse = await WebRequest.get(searchQuery.replace("0", String(randomIndex)))
+        const randomResponse = await WebRequest.get(searchQuery.replace(defaultStartIndex, String(randomIndex)))
         const randomUri = (<FoobySearchResponse>JSON.parse(randomResponse.content)).results.find(res => !!res.url).url
         console.debug(randomUri)
 
         const recipeSite = await WebRequest.get(randomUri)
 
-        const $ = cheerio.load(recipeSite.content)
-        const ingredientResponse = <IngredientResponse> $('[data-portion-calculator-initial-all-ingredients]').data().portionCalculatorInitialAllIngredients
-        console.log(ingredientResponse.ingredients.map(ing => ing.desc).join("\n"));
-        console.log($('meta[property="og:title"]').attr('content'))
-        console.log($('meta[property="og:image"]').attr('content'))
+        const cheerioLoaded = cheerio.load(recipeSite.content)
+        const ingredientResponse = <IngredientResponse>cheerioLoaded('[data-portion-calculator-initial-all-ingredients]').data().portionCalculatorInitialAllIngredients
+        const recipeCard = <RecipeCard>{
+            title: cheerioLoaded('meta[property="og:title"]').attr('content'),
+            imageUrl: cheerioLoaded('meta[property="og:image"]').attr('content'),
+            ingredients: ingredientResponse.ingredients
+        }
+
+        console.log(recipeCard.title)
+        console.log(recipeCard.imageUrl)
+        console.log(recipeCard.ingredients.map(i => i.desc).join("\n"))
+
+
+        return recipeCard
     }
 }
 
 const app = new App()
+console.log("test")
 
-app.display().then()
+app.getRandomRecipeCard()
+// .then(recipeCard => {
+//     jQuery('#title').html(recipeCard.html)
+//     jQuery('#recipe-image').src(recipeCard.imageUrl)
+//     jQuery('#ingredients').html(recipeCard.map((i: Ingredient) => i.desc))
+//     console.log(recipeCard)
+// })
 
-interface IngredientResponse{
+interface RecipeCard {
+    title: string
+    imageUrl: string
     ingredients: Ingredient[]
 }
 
-interface Ingredient{
+interface IngredientResponse {
+    ingredients: Ingredient[]
+}
+
+interface Ingredient {
     quantity: number
     measure: string
     desc: string
